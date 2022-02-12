5516ea3ab2b92eb3b0103ec4552bc12e4c7af1d2
joey-dev <joeystil3@gmail.com>
2022-02-12 01:35:33 +0100

    changed interface name is the language keys and exported it. added export function to get the languages, and the correct language translation for a item with english-us as default

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
+import enUS from './en-US/index.json';
+import nlNL from './nl-NL/index.json';
+
+export const languages: ILanguages = {
+    enUS: enUS,
+    nlNL: nlNL,
+};
+
+export const getItemTranslate = (languageTranslationList: ILanguages, languageName: keyof ILanguages, translatedTextName: keyof ILanguage): string => {
+    const translationsList: ILanguage = languageTranslationList[languageName];
+    if (translationsList.hasOwnProperty(translatedTextName)) {
+        return translationsList[translatedTextName] as string;
+    }
+
+    return languageTranslationList['enUS'][translatedTextName] as string;
+};
+
 export interface ILanguages {
-    enUS: Language;
-    nlNL: Language;
+    enUS: ILanguage;
+    nlNL: ILanguage;
 }
 
-interface Language {
+export interface ILanguage {
     LOGIN_BUTTON?: string;
     LOGOUT?: string;
     NO_SERVER_SELECTED?: string;
@@ -41,14 +58,5 @@ interface Language {
     SETTINGS_DELETE_REPLY_SECONDS?: string;
 
     SETTINGS_EPHEMERAL_TITLE?: string;
-    SETTINGS_EPHEMERAL_DESCRIPTION?: string
-
+    SETTINGS_EPHEMERAL_DESCRIPTION?: string;
 }
-import enUS from './en-US/index.json';
-import nlNL from './nl-NL/index.json';
-
-export const languages: ILanguages = {
-    enUS: enUS,
-    nlNL: nlNL,
-};
-
